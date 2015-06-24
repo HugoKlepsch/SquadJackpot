f8730db01f338f0aa9533e141741068df8c40984
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-06-23 22:12:58 -0400

    Host button now starts host activity.

--- app/src/main/java/hugra/squadjackpot/SelectGameModeActivity.java
+++ app/src/main/java/hugra/squadjackpot/SelectGameModeActivity.java
@@ -1,5 +1,6 @@
 package hugra.squadjackpot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -38,13 +39,15 @@ public class SelectGameModeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void join(View v){
-        Log.d("sgm", "in join");
-        //startActivity();
-    }
-
     public void host(View v){
         Log.d("sgm", "in host");
+        startActivity(new Intent(this, HostActivity.class));
     }
+
+    public void join(View v){
+        Log.d("sgm", "in join");
+    }
+
+
 
 }
