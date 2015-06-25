362c188f5573769b6b07385fbf909d942d613515
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-06-25 13:15:58 -0400

    Added some basic nfc code to the joinactivity. Makes a asample message and also accepts one.

--- app/src/main/java/hugra/squadjackpot/JoinActivity.java
+++ app/src/main/java/hugra/squadjackpot/JoinActivity.java
@@ -1,15 +1,21 @@
 package hugra.squadjackpot;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import static android.nfc.NdefRecord.createMime;
 
 
-public class JoinActivity extends Activity {
+public class JoinActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +23,40 @@ public class JoinActivity extends Activity {
         setContentView(R.layout.activity_join);
         NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) return;  // NFC not available on this device
-        nfcAdapter.setNdefPushMessage(new NdefMessage(NdefRecord.createApplicationRecord("dong")), this);
 
+        nfcAdapter.setNdefPushMessageCallback(this, this);
     }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Check to see that the Activity started due to an Android Beam
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    /**
+     * Parses the NDEF Message from the intent and prints to the TextView
+     */
+    void processIntent(Intent intent) {
+        TextView textView = (TextView) findViewById(R.id.joinActivity_HostNameTextView);
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
+                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        // only one message sent during the beam
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        textView.setText(new String(msg.getRecords()[0].getPayload()));
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,9 +80,23 @@ public class JoinActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String header = "Beam message \n\nCurrent Time: " + System.currentTimeMillis();
+        return new NdefMessage(new NdefRecord[]{
+                createMime(
+                        "application/vnd.com.example.android.beam", header.getBytes())
+                /**
+                 * The Android Application Record (AAR) is commented out. When a device
+                 * receives a push with an AAR in it, the application specified in the AAR
+                 * is guaranteed to run. The AAR overrides the tag dispatch system.
+                 * You can add it back in to guarantee that this
+                 * activity starts when receiving a beamed message. For now, this code
+                 * uses the tag dispatch system.
+                 */
+                ,NdefRecord.createApplicationRecord("hugra.squadjackpot")
 
-
-    public void setNdefPushMessage (NdefMessage message, Activity activity, Activity... activities){
+        });
 
     }
 }
